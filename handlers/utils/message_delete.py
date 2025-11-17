5609a87416540ffe0eab14b54863d01bdd3b3f40
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2025-11-17 16:57:31 +0530

    Update message_delete.py

--- handlers/utils/message_delete.py
+++ handlers/utils/message_delete.py
@@ -1,8 +1,10 @@
-# © @TheAlphaBotz [2021-2025]
+# -- coding: utf-8 --
+
 import asyncio
 import logging
 from pyrogram import Client, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import config
 
 logger = logging.getLogger(__name__)
 
@@ -10,14 +12,18 @@ async def schedule_message_deletion(client: Client, chat_id: int, msg_ids: list,
     await asyncio.sleep(mins * 60)
     try:
         await client.delete_messages(chat_id, msg_ids)
+
         kb = None
         if link:
+            
+            start_link = f"https://t.me/{config.BOT_NAME}?start={link}"
             kb = InlineKeyboardMarkup(
-                [[InlineKeyboardButton("🔄 Get Again", url=link)]]
+                [[InlineKeyboardButton("🔄 Get Again", url=start_link)]]
             )
+
         await client.send_message(
             chat_id,
-            "<blockquote>This file was auto-deleted after the set time to save space.\n"
+            "<blockquote>🗑️ This file was auto-deleted after the set time to save space.\n"
             "Use the button below to get it again.</blockquote>",
             reply_markup=kb,
             parse_mode=enums.ParseMode.HTML
